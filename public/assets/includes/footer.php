b1ad30244eb3b3e436f28d64c3b2ec3a4dcb2f9b
Rik Kendell <rik.kendell@gmail.com>
2013-01-15 18:09:06 +0000

    Structural changes and minor styling

---
+++ public/assets/includes/footer.php
@@ -0,0 +1,29 @@
+        </div><!-- container -->
+
+        <footer id="footer-wrap">
+            <div class="container">
+                <p>@name</p>
+                <p>I'm a twenty-something UX developer and designer living in Leeds, UK. I love semantic code, great design, good food and robots. I make up one half of <a href="http://www.waffleworks.co/" title="_blank" title="Waffle Works">Waffle Works</a>.</p>
+                
+                <p>&copy; Copyright @name @current_year <a class="atom-rss" href="@root_path/?/feed/">RSS</a></p>
+            </div>
+        </footer>
+
+        <!-- Scripts -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="@root_path/public/assets/js/libs/jquery-1.8.0.min.js"><\/script>')</script>
+
+        <script src="@root_path/public/assets/js/plugins.js"></script>
+        <script src="@root_path/public/assets/js/assets/js/scripts.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+
+    </body>
+
+</html>
