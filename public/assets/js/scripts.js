57d124bd6f51867aa9f441c7daf5337d49f7aae5
Rik Kendell <rik.kendell@gmail.com>
2013-06-04 16:44:02 +0100

    New Articles & tweaks
    - Three new articles (IE10 detection, Royalslider issue, Installing
    Sass)
    - Twitter feed set-up
    - Amends to Header / Footer and styling
    - Moved Intro text to sidebar

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,2 +1,22 @@
+$(document).ready(function() {
 
-SyntaxHighlighter.all()
+	SyntaxHighlighter.all()
+
+	$('#twitter-feed').jTweetsAnywhere({
+        username: 'Rikki_Sixx',
+        count: 3,
+        showTwitterBird: false,
+        showTweetFeed: {
+            autorefresh: {
+                mode: 'trigger-insert',
+                interval: 60
+            },
+            paging: {
+                mode: 'more'
+            },
+            showTimestamp: {
+                refreshInterval: 15
+            }
+        }
+    });
+});
